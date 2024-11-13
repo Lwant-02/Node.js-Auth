b508ca87b42d80cbfa7d8a30251636f569b5ced9
Lwant <jaimain671@gmail.com>
2024-11-13 12:55:55 +0700

    Repush the project

--- server/main.js
+++ server/main.js
@@ -19,9 +19,9 @@ app.use(cookieParser());
 app.use("/api/auth", authRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../client/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../Frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"));
   });
 }
 
